f47e8747d7074a0539f000e2d9fea96b22b49f8a
metinsanli <metinsanli@gmail.com>
2014-03-13 16:59:22 +0200

    Cok boyutlu dizi ornekleri eklendi.

---
+++ ArrayCokBoyutlu2.java
@@ -0,0 +1,13 @@
+public class ArrayCokBoyutlu2 {
+
+	public static void main(String[] args) {
+		int[][] dizi2D = { { 1, 2 }, { 4, 5, 6 }, { 7, 8 }, { 10, 11, 12 } };
+		System.out.println("dizi2D uzunlugu = " + dizi2D.length);
+
+		for (int i = 0; i < dizi2D.length; i++) {
+			for (int j = 0; j < dizi2D[i].length; j++)
+				System.out.print("\ndizi2D [" + i + "][" + j + "]=" + dizi2D[i][j]);
+			System.out.println();
+		}
+	}
+}
---
+++ ArrayCokBoyutlu4Tanimlamalar.java
@@ -0,0 +1,20 @@
+public class ArrayCokBoyutlu4Tanimlamalar {
+
+	public static void main(String[] args) {
+
+		// en az bir boyutun insdis adedi belirtilmelidir.
+		int[][] sayfa = new int[6][];
+
+		// difer boyutu sonrasinda tanimlamamiz gerekir.
+		for (int i = 0; i < sayfa.length; i++)
+			sayfa[i] = new int[5];
+
+		// her iki boyutuda gelismis for ile yazdir
+		for (int[] satir : sayfa) {
+			for (int hucre : satir)
+				System.out.print(hucre + " ");
+			System.out.println();
+		}
+
+	} // end main
+} // end class
