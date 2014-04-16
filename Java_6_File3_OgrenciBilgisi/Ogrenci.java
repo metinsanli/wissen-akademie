1244df0f3848eb4cbc365227db11569cf3cef605
metinsanli <metinsanli@gmail.com>
2014-04-16 23:37:23 +0300

    Ogrenci Bilgi Sistemi Uygulamasi [BETA2] eklendi.

---
+++ Java_6_File3_OgrenciBilgisi/Ogrenci.java
@@ -0,0 +1,12 @@
+public class Ogrenci {
+
+	String no, adi, soyadi;
+	int gunSay;
+
+	public Ogrenci(String no, String adi, String soyadi) {
+		this.no = no;
+		this.adi = adi;
+		this.soyadi = soyadi;
+	} // end constructor
+
+} // end class
