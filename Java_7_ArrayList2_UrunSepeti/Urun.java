3ba39b7973f022fbe0499292f6f9a59c31be51f8
metinsanli <metinsanli@gmail.com>
2014-04-21 13:56:03 +0300

    ArrayList [2] eklendi.

---
+++ Java_7_ArrayList2_UrunSepeti/Urun.java
@@ -0,0 +1,22 @@
+public class Urun {
+
+	private String urunAdi;
+	private int urunAdet;
+
+	public String getUrunAdi() {
+		return urunAdi;
+	}
+
+	public void setUrunAdi(String urunAdi) {
+		this.urunAdi = urunAdi;
+	}
+
+	public int getUrunAdet() {
+		return urunAdet;
+	}
+
+	public void setUrunAdet(int urunAdet) {
+		this.urunAdet = urunAdet;
+	}
+
+} // end class
