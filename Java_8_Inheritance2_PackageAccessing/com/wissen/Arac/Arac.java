e8e64b627643e1daefe7660024da6b9e8ca5a19e
metinsanli <metinsanli@gmail.com>
2014-04-28 19:06:08 +0300

    Kalitim konusuna paralel konu paket erisim ormegi eklendi.

---
+++ Java_8_Inheritance2_PackageAccessing/com/wissen/Arac/Arac.java
@@ -0,0 +1,10 @@
+package com.wissen.Arac;
+
+public class Arac extends Object {
+
+	int defaultArac;
+	private int privateArac;
+	protected int protectedArac;
+	public int publicArac;
+
+}
---
+++ Java_8_Inheritance2_PackageAccessing/com/wissen/Otobus/Otobus.java
@@ -0,0 +1,12 @@
+package com.wissen.Otobus;
+
+import com.wissen.Arac.Arac;
+
+public class Otobus extends Arac {
+
+	public Otobus() {
+		// arac sinifindan sadece protected public
+		this.protectedArac = 0;
+		this.publicArac = 0;
+	}
+}
