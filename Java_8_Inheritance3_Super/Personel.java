b9b146d3f056176de7438bcb99214dbcd9abfc72
metinsanli <metinsanli@gmail.com>
2014-04-28 19:08:05 +0300

    Kalitim konusuna paralel super constructor ornegi eklendi.

---
+++ Java_8_Inheritance3_Super/Personel.java
@@ -0,0 +1,17 @@
+public class Personel {
+
+	protected String ad;
+	protected String soyad;
+	public String tcKimlik;
+	private Double sabitMaas;
+
+	public Personel() {
+	}
+
+	public Personel(String persAd, String persSoyad, double sabitMaas) {
+		this.ad = persAd;
+		this.soyad = persSoyad;
+		this.sabitMaas = sabitMaas;
+	}
+
+}
---
+++ Java_8_Inheritance3_Super/SatisPersoneli.java
@@ -0,0 +1,16 @@
+public class SatisPersoneli extends Personel {
+	public double satisTutari;
+	public double primYuzdesi;
+
+	public SatisPersoneli() {
+		super(); // extend edilen sinifin constructor'ina erismek icin
+	}
+
+	public SatisPersoneli(String ptpAd, String ptpSoyad, double satisTutari,
+			double prim) {
+		super(ptpAd, ptpSoyad, 500.0);
+		this.satisTutari = satisTutari;
+		this.primYuzdesi = prim;
+	}
+
+}
