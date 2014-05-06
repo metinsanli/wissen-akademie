e3373ee00ec868d36859d5dceb03821075e58650
metinsanli <metinsanli@gmail.com>
2014-05-06 22:27:04 +0300

    Elements 1  eklendi.

---
+++ Java_14_Swing1_Elements1/P04/LabelveIconEkleme/Swing4_LabelFrameUygulama.java
@@ -0,0 +1,15 @@
+package P04.LabelveIconEkleme;
+import javax.swing.JFrame;
+
+public class Swing4_LabelFrameUygulama {
+
+	public static void main(String[] args) {
+
+		Swing4_LabelFrame labelFrame = new Swing4_LabelFrame();
+		labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		labelFrame.setSize(560, 380);
+		labelFrame.setVisible(true);
+
+	}
+
+}
