6ed514e530fd0999755142302f5036fd87a601bb
metinsanli <metinsanli@gmail.com>
2014-05-08 22:09:17 +0300

    Border ve Grid Layout ornekleri eklendi.

---
+++ Java_14_Swing7_Layout1/P01_BorderVeGrid/Dosya.java
@@ -0,0 +1,68 @@
+package P01_BorderVeGrid;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+@SuppressWarnings("all")
+public class Dosya {
+
+	private static File dosya = new File("./src/kayit.txt");
+
+	private static BufferedReader bufOku(File dosya) throws Exception {
+		return new BufferedReader(new InputStreamReader(new FileInputStream(dosya), "Cp1254"));
+	} // end method bufOku()
+
+	private static BufferedWriter bufYaz(File dosya) throws Exception {
+		return new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dosya, true), "Cp1254"));
+	} // end method bufYaz()
+
+	public String bul(String aramaKriteri) throws Exception {
+		BufferedReader oku = bufOku(this.dosya);
+		String satir = null, kelimeler[];
+		while ((satir = oku.readLine()) != null) {
+			kelimeler = satir.split("\t");
+			if (kelimeler[0].equalsIgnoreCase(aramaKriteri))
+				break;
+			else
+				satir = null;
+		}
+		oku.close();
+		return satir;
+	} // end method bul()
+
+	public void ekle(String yeniSatir) throws Exception {
+		BufferedWriter yaz = bufYaz(this.dosya);
+		yaz.write(yeniSatir);
+		yaz.newLine();
+		yaz.close();
+	} // end method ekle()
+
+	public void guncelle(String aramaKriteri, String yeniSatir) throws Exception {
+		String[] varmi = bul(aramaKriteri).split("\t");
+		if (aramaKriteri.equalsIgnoreCase(varmi[0])) {
+			File tmp = new File("./src/temp.txt");
+			BufferedReader oku = bufOku(this.dosya);
+			BufferedWriter yaz = bufYaz(tmp);
+			String satir, kelimeler[];
+			while ((satir = oku.readLine()) != null) {
+				kelimeler = satir.split("\t");
+				if (kelimeler[0].equalsIgnoreCase(aramaKriteri)) {
+					yaz.write(yeniSatir);
+					yaz.newLine();
+				} else {
+					yaz.write(satir);
+					yaz.newLine();
+				}
+			}
+			oku.close();
+			yaz.close();
+			dosya.delete();
+			tmp.renameTo(dosya);
+		}
+	} // end method guncelle()
+
+} // END CLASS
---
+++ Java_14_Swing7_Layout1/P02_BorderLayout/BL.java
@@ -0,0 +1,34 @@
+package P02_BorderLayout;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+@SuppressWarnings("all")
+public class BL extends JFrame {
+
+	private Container c = getContentPane();
+
+	public BL() {
+		super("hey");
+		setLayout(new BorderLayout());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(300, 400);
+		// Pencereyi ekran boyutuna gore yeniden yerlestir
+		Toolkit toolkit = getToolkit();
+		Dimension size = toolkit.getScreenSize();
+		setLocation((size.width / 2) - (getWidth() / 2), (size.height / 2) - (getHeight() / 2));
+
+		add(new JButton("East"), BorderLayout.EAST);
+		add(new JButton("West"), BorderLayout.WEST);
+		add(new JButton("North"), BorderLayout.NORTH);
+		add(new JButton("South"), BorderLayout.SOUTH);
+
+		setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new BL();
+	}
+}
