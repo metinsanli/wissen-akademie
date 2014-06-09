aa038f6d8ed4dae6cb5d666f7f9fc9dfaa015529
metinsanli <metinsanli@gmail.com>
2014-06-09 18:19:22 +0300

    Do-While Döngüsü örnekleri eklendi.

---
+++ Java_01_Basic/E05_DoWhileDongusu/DoWhileYapisi3IcindeWhile.java
@@ -0,0 +1,27 @@
+package E05_DoWhileDongusu;
+public class DoWhileYapisi3IcindeWhile {
+
+	public static void main(String[] args) {
+		int sayac1 = 500, sayac2;
+
+		DisDongu: do {
+			System.out.println(sayac1);
+			sayac1++;
+			sayac2 = 0; // ic dongunun sayacini sifirla
+
+			while (sayac2 <= 9) {
+				if (sayac2 == 5) {
+					sayac2++;
+					continue; // continue ic dongu
+				}
+
+				if (sayac1 == 505)
+					break DisDongu;
+
+				System.out.println("Sayac = " + sayac2);
+				sayac2++;
+			} // end IC do-while
+		} while (sayac1 <= 515); // end DIS do-while
+
+	} // end main
+} // end class
