ab4b1f9bf1ab75833a976a0a04ef8eccdc25d2e9
metinsanli <metinsanli@gmail.com>
2014-06-12 20:19:14 +0300

    String sınıfı için Kelime Sayaç örneği eklendi.

---
+++ Java_05_StringClass/E02_KelimeSayac/Kelime.java
@@ -0,0 +1,51 @@
+package E02_KelimeSayac;
+
+@SuppressWarnings("all")
+public class Kelime {
+
+	static int kelimeToplamAdet;
+	static String[] ayniKelimeler;
+	static int[] ayniKelimeAdet;
+
+	public Kelime(String[] metin) {
+
+		// KAC TANE KELIME VAR BUL
+		kelimeToplamAdet = metin.length;
+
+		// NOKTA VE VIRGUL AYIKLA
+		noktalamaTemizle(metin);
+
+		// AYNI KELIMELERI BUL VE SAY
+		ayniKelimeleriBul(metin);
+
+	} // end constructor
+
+	public static void noktalamaTemizle(String[] metin) {
+		for (int i = 0; i < metin.length; i++)
+			if (metin[i].endsWith(".") || metin[i].endsWith(",") || metin[i].endsWith("\"") || metin[i].startsWith("\"")) {
+				metin[i] = metin[i].replace('.', ' ').trim();
+				metin[i] = metin[i].replace(',', ' ').trim();
+				metin[i] = metin[i].replace('\"', ' ').trim();
+			}
+	} // end method noktalamaTemizle()
+
+	public static void ayniKelimeleriBul(String[] metin) {
+		ayniKelimeler = new String[metin.length];
+		ayniKelimeAdet = new int[metin.length];
+		int sayac = 0;
+		for (int i = 0; i < metin.length; i++) {
+			boolean bulundu = false;
+			for (int j = 0; j < ayniKelimeler.length; j++) {
+				if (metin[i].equalsIgnoreCase(ayniKelimeler[j])) {
+					ayniKelimeAdet[i]++;
+					bulundu = true;
+					break;
+				}
+			} // end for j
+			if (!bulundu) {
+
+			}
+		} // end for i
+	} // end method ayniKelimeleriBul()
+
+} // end class
