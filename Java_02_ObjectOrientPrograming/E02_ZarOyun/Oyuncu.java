fa241ec98e283c827e9ff0eab806913372404dd8
metinsanli <metinsanli@gmail.com>
2014-06-12 16:13:30 +0300

    Object Orient Programing için Zar oyunu örneği eklendi.

---
+++ Java_02_ObjectOrientPrograming/E02_ZarOyun/Oyuncu.java
@@ -0,0 +1,25 @@
+package E02_ZarOyun;
+public class Oyuncu {
+	public String adi;
+	public int skor;
+	public int yas;
+	public Zar zar = new Zar(); // eger new ile baslatilmazsa eclipse hata
+								// vermez fakat runtimeda NullPointerException
+								// hatasi alinir.
+
+	public void isimAl(String isim, int yas) {
+		this.adi = isim;
+		this.yas = yas;
+	} // end isimAl()
+
+	public void isimAl() {
+		this.adi = "Sercan";
+		this.yas = 26;
+	} // end isimAl()
+
+	public void zarSalla(int sallamaSayisi) {
+		for (int i = 0; i < sallamaSayisi; i++)
+			this.skor += zar.zarAt();
+	} // end zarSalla()
+
+} // end class
