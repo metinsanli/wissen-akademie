d7b9253401e8672bcb75cc7b0057036bf6a35d0c
metinsanli <metinsanli@gmail.com>
2014-06-12 20:19:42 +0300

    String sınıfı için Kelime Sayaç örneği eklendi. [org]

---
+++ Java_05_StringClass/E02_KelimeSayac_VolkanHoca/Kelime.java
@@ -0,0 +1,16 @@
+package E02_KelimeSayac_VolkanHoca;
+
+public class Kelime {
+	String deger;
+	int adet;
+	
+	static int kelimeSayisi;//ka� farkl� kelime oldu�unu saklar
+	
+	public void temizle(){
+		
+	}
+	public static String temizle(String ifade){
+		//temizleme i�lemlerini yap
+		return ifade;
+	}
+}
