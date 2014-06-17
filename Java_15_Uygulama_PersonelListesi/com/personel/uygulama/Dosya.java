57bbf3423d6a343c07a2d1b7ba3f59ba5a79cf10
metinsanli <metinsanli@gmail.com>
2014-06-17 19:30:53 +0300

    Object Serialization ve File konularını içeren örnek eklendi.

---
+++ Java_15_Uygulama_PersonelListesi/com/personel/uygulama/Dosya.java
@@ -0,0 +1,50 @@
+package com.personel.uygulama;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+@SuppressWarnings("all")
+public class Dosya {
+
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+
+	public void yaz(ArrayList<Personel> personel) {
+		try {
+			output = new ObjectOutputStream(new FileOutputStream("./src/com/personel/dosya/personel.ser"));
+			for (Personel p : personel)
+				output.writeObject(p);
+			output.close();
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+	} // end method yaz()
+
+	public ArrayList<Personel> oku() {
+		ArrayList<Personel> personeller = new ArrayList<Personel>();
+		try {
+			input = new ObjectInputStream(new FileInputStream("./src/com/personel/dosya/personel.ser"));
+			while (true) {
+				personeller.add((Personel) input.readObject());
+			}
+		} catch (EOFException e) {
+			try {
+				input.close();
+			} catch (IOException e1) {
+			}
+			return personeller;
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		} catch (ClassNotFoundException e) {
+		}
+		return personeller;
+	} // end method oku()
+
+} // END CLASS
---
+++ Java_15_Uygulama_PersonelListesi/com/personel/uygulama/Personel.java
@@ -0,0 +1,71 @@
+package com.personel.uygulama;
+
+import java.io.Serializable;
+
+@SuppressWarnings("all")
+public class Personel implements Serializable {
+
+	private String tc, adSoyad, meslek, telefon, sehir;
+
+	public Personel(String tc, String adSoyad, String meslek, String telefon, String sehir) {
+		setTc(tc);
+		setAdSoyad(adSoyad);
+		setMeslek(meslek);
+		setTelefon(telefon);
+		setSehir(sehir);
+	} // end constructor
+
+	public Personel(String[] personelBilgileri) {
+		setTc(personelBilgileri[0]);
+		setAdSoyad(personelBilgileri[1]);
+		setMeslek(personelBilgileri[2]);
+		setTelefon(personelBilgileri[3]);
+		setSehir(personelBilgileri[4]);
+	}// end constructor
+
+	public String[] getAll() {
+		String[] tmp = { getTc(), getAdSoyad(), getMeslek(), getTelefon(), getSehir() };
+		return tmp;
+	} // end method getAll()
+
+	public String getTc() {
+		return tc;
+	}
+
+	public void setTc(String tc) {
+		this.tc = tc;
+	}
+
+	public String getAdSoyad() {
+		return adSoyad;
+	}
+
+	public void setAdSoyad(String adSoyad) {
+		this.adSoyad = adSoyad;
+	}
+
+	public String getMeslek() {
+		return meslek;
+	}
+
+	public void setMeslek(String meslek) {
+		this.meslek = meslek;
+	}
+
+	public String getTelefon() {
+		return telefon;
+	}
+
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+
+	public String getSehir() {
+		return sehir;
+	}
+
+	public void setSehir(String sehir) {
+		this.sehir = sehir;
+	}
+
+} // END CLASS
