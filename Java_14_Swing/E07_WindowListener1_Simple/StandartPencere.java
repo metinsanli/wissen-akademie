27110d57e4eefb43363ed24aea5cc025feb34c7e
metinsanli <metinsanli@gmail.com>
2014-06-17 19:19:07 +0300

    Window Listener örneği eklendi.

---
+++ Java_14_Swing/E07_WindowListener1_Simple/StandartPencere.java
@@ -0,0 +1,21 @@
+package E07_WindowListener1_Simple;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class StandartPencere extends JFrame {
+
+	public StandartPencere(String pencereBasligi) {
+		super(pencereBasligi);
+		setLayout(new FlowLayout());
+		this.setSize(550, 300);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		Toolkit toolkit = getToolkit();
+		Dimension size = toolkit.getScreenSize();
+		this.setLocation(size.width / 2 - getWidth() / 2, size.height / 2 - getHeight() / 2);
+	} // end constructor
+
+} // end class
